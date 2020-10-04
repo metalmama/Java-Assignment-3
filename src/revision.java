66768f223ba09bf0ed9ff131b7d02bf08db8adf8
metalmama <sofie@callycode.com>
2020-10-04 17:59:36 +0200

    Pushing it to the branch

--- src/revision.java
+++ src/revision.java
@@ -1,14 +1,30 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class revision {
     public static void main(String[] args) {
         try
         {
             int data=50/0; //may throw exception
+            System.out.println("This one never appears on the console");
         }
         //handling the exception
-        catch(ArithmeticException e)
+        catch(Exception e)
         {
-            System.out.println(e);
+            System.out.println("The error is" + " " + e.getMessage());
         }
-        System.out.println("rest of the code");
+        System.out.println("Continuing with the rest of the code");
+        moreJuice();
     }
-}
+
+    public static void moreJuice() {
+        String brand = "";
+        String flavour = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a brand");
+        brand = scanner.nextLine();
+        System.out.println("Enter a flavour");
+        flavour = scanner.nextLine();
+        System.out.println(brand +" " + flavour + " is served");
+        }
+    }
